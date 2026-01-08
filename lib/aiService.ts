0aeb225b3e8584bdacd5f67d62d80b6babc7233f
Asyrofuddien <asyrofuddien11@gmail.com>
2026-01-08 12:25:58 +0700

    Update aiService.ts

--- lib/aiService.ts
+++ lib/aiService.ts
@@ -1,5 +1,5 @@
 // const BACKEND_URL = 'http://146.190.107.186:3500';
-const BACKEND_URL = 'http://localhost:8001';
+const BACKEND_URL = 'http://146.190.107.186:9000';
 // const BACKEND_URL = 'http://146.190.107.186:9000';
 // const BACKEND_URL = 'https://social-ghosts-know.loca.lt';
 
